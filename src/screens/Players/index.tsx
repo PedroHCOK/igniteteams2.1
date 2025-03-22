914d7d505d156641615cc5d97fc70f8a6cbdb278
Pedro Oliveira <docpedro.ar@gmail.com>
2025-03-21 23:31:19 -0300

    added dynamism to the ButtonIcon and repositioned

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,8 +1,9 @@
+import { Input } from '@components/Input';
 import { Header } from '@components/Header';
 import { Highlight } from '@components/Highlight/Index';
 import { ButtonIcon } from '@components/ButtonIcon';
 
-import { Container } from './styles';
+import { Container, Form } from './styles';
 
 export function Players(){
     return (
@@ -14,7 +15,17 @@ export function Players(){
                 subtitle="adicione a galera e separe os times"
             />
 
-            <ButtonIcon/>
+            <Form>
+                <Input
+                    placeholder='Nome da pessoa'
+                    autoCorrect={false}
+                />
+                <ButtonIcon
+                    icon='add'
+                 />
+            </Form>
+                
+            
 
         </Container>
     );
